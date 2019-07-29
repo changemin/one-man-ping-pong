d8401c9029d723cc027a6befc0666286b9edc7b4
Changemin <bkm.change.min@gmail.com>
2019-07-29 18:29:48 +0900

    패들 객체 생성

--- main.py
+++ main.py
@@ -5,33 +5,47 @@ from pygame.locals import *
 
 pygame.init()
 
+colors = {
+    "background":[250,227,217],
+    "ball":[255,182,185],
+    "paddle":[187,222,214]
+}
 #게임 플레이에 주축이 되는 변수들 정의
-WINDOW_WIDTH = 400
+WINDOW_WIDTH = 600
 WINDWO_HEIGHT = 600
-backgroundcolor = [255,255,255]
 ballX = random.randrange(0, WINDOW_WIDTH)
 ballY = random.randrange(0, WINDWO_HEIGHT)
-ballXSpd = 10
-ballYSpd = 10
+ballXSpd = 5
+ballYSpd = 5
 radius = 10
 windowSize = [WINDOW_WIDTH,WINDWO_HEIGHT] #창 크기 설정
 screen = pygame.display.set_mode(windowSize) #screen 생성
-FPS = 30
+FPS = 60
+
 clock = pygame.time.Clock()
 Running = True
 pygame.display.set_caption("One Man Ping Pong")
 
+class Paddle:
+    def __init__(self, location, length, thickness):
+        self.location = location
+        self.length = length
+        self.thickness = thickness
+
+PaddleTOP = Paddle(random.randrange(0,WINDOW_WIDTH),50,10)
 while True:
-    screen.fill(backgroundcolor)
-    pygame.draw.circle(screen, (0,0,255), (ballX, ballY), radius)
-    pygame.draw.circle(screen, backgroundcolor, (ballX, ballY), radius)
+    screen.fill(colors["background"])
+    pygame.draw.circle(screen, colors["ball"], (ballX, ballY), radius)
+    pygame.draw.circle(screen, colors["background"], (ballX, ballY), radius) #다시 지우기
+    pygame.draw.line(screen, colors["paddle"],[PaddleTOP.location-(PaddleTOP.length/2),30],[PaddleTOP.location+(PaddleTOP.length/2),30],30)
     ballX += ballXSpd
     ballY += ballYSpd
+    print("X:" + str(ballX) +" Y:"+str(ballY))
     if(ballX + radius >= WINDOW_WIDTH or ballX - radius <= 0): #벽 충돌 이벤트 검사
         ballXSpd *= -1
     if(ballY + radius >= WINDWO_HEIGHT or ballY - radius <= 0): #벽 충돌 이벤트 검사
         ballYSpd *= -1
-    pygame.draw.circle(screen, (0,0,255), (ballX, ballY), radius)
+    pygame.draw.circle(screen, colors["ball"], (ballX, ballY), radius)
     for event in pygame.event.get(): #event get
         if not hasattr(event, 'key'):
             continue
