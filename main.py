01ed2f4e7b5883cb99b55e15ac9d3090d68fe20d
Changemin <bkm.change.min@gmail.com>
2019-08-03 18:34:21 +0900

    상, 하, 좌, 우 패들 충돌 검사 구현

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 windowSize = [WINDOW_WIDTH,WINDOW_HEIGHT] #창 크기 설정
 screen = pygame.display.set_mode(windowSize) #screen 생성
-FPS = 60
+FPS = 10
 
 FPSclock = pygame.time.Clock()
 Running = True
@@ -32,17 +32,39 @@ class Ball:
         self.x += self.XSpd
         self.y += self.YSpd
     def collisionCheck(self):
+        pygame.draw.circle(screen, (255,255,255), (75,75), self.radius)
+        pygame.draw.circle(screen, (255,255,255), (75,125), self.radius)
+        pygame.draw.circle(screen, (255,255,255), (50,100), self.radius)
+        pygame.draw.circle(screen, (255,255,255), (100,100), self.radius)
         if(self.x + self.radius >= WINDOW_WIDTH or self.x - self.radius <= 0): #벽 충돌 이벤트
             self.XSpd *= -1
             self.WallColli += 1
         elif(self.y + self.radius >= WINDOW_HEIGHT or self.y - self.radius <= 0): #벽 충돌 이벤트
             self.YSpd *= -1
             self.WallColli += 1
-        elif(self.x >= PaddleTOP.x - PaddleTOP.length/2 and self.x <= PaddleTOP.x + PaddleTOP.length/2): #상단 바와 충돌 검사
-            pygame.draw.circle(screen, (255,0,0), (50,50), self.radius)
+        elif(self.x >= PaddleTOP.x - PaddleTOP.length/2 and self.x <= PaddleTOP.x + PaddleTOP.length/2): #상단 패들과 충돌 검사
+            pygame.draw.circle(screen, (255,0,0), (75,75), self.radius)
             if(self.y - self.radius <= PaddleTOP.y + PaddleTOP.thickness/2):
-                pygame.draw.circle(screen, (0,0,255), (100,50), self.radius)
+                pygame.draw.circle(screen, (0,0,255), (75,75), self.radius)
                 self.YSpd *= -1
+                self.WallColli += 1
+        elif(self.x >= PaddleBOTTOM.x - PaddleBOTTOM.length/2 and self.x <= PaddleBOTTOM.x + PaddleBOTTOM.length/2): #하단 패들과 충돌 검사
+            pygame.draw.circle(screen, (255,0,0), (75,125), self.radius)
+            if(self.y + self.radius >= PaddleBOTTOM.y - PaddleBOTTOM.thickness/2):
+                pygame.draw.circle(screen, (0,0,255), (75,125), self.radius)
+                self.YSpd *= -1
+                self.WallColli += 1
+        elif(self.y >= PaddleLEFT.y - PaddleLEFT.length/2 and self.y <= PaddleLEFT.y + PaddleLEFT.length/2): #왼쪽 패들과 충돌 검사
+            pygame.draw.circle(screen, (255,0,0), (50,100), self.radius)
+            if(self.x - self.radius <= PaddleLEFT.x + PaddleLEFT.thickness/2):
+                pygame.draw.circle(screen, (0,0,255), (50,100), self.radius)
+                self.XSpd *= -1
+                self.WallColli += 1
+        elif(self.y >= PaddleRIGHT.y - PaddleRIGHT.length/2 and self.y <= PaddleRIGHT.y + PaddleRIGHT.length/2): #오른쪽 패들과 충돌 검사
+            pygame.draw.circle(screen, (255,0,0), (50,100), self.radius)
+            if(self.x + self.radius >= PaddleRIGHT.x - PaddleRIGHT.thickness/2):
+                pygame.draw.circle(screen, (0,0,255), (50,100), self.radius)
+                self.XSpd *= -1
                 self.WallColli += 1
 
 class Paddle:
@@ -55,16 +77,16 @@ class Paddle:
         self.x = mousex
         pygame.draw.line(screen, colors["paddle"],[self.x-(self.length/2),self.y],[self.x+(self.length/2),self.y],self.thickness)
     def drawBOTTOM(self):
-        self.x = mousex
-        pygame.draw.line(screen, colors["paddle"],[WINDOW_WIDTH-(self.x-(self.length/2)),self.y],[WINDOW_WIDTH-(self.x+(self.length/2)),self.y],self.thickness)
+        self.x = WINDOW_WIDTH-mousex
+        pygame.draw.line(screen, colors["paddle"],[self.x-(self.length/2),self.y],[self.x+(self.length/2),self.y],self.thickness)
     def drawLEFT(self):
         self.y = mousey
         pygame.draw.line(screen, colors["paddle"],[self.x,self.y-(self.length/2)],[self.x,self.y+(self.length/2)],self.thickness)
     def drawRIGHT(self):
-        self.y = mousey
-        pygame.draw.line(screen, colors["paddle"],[self.x,WINDOW_HEIGHT-(self.y-(self.length/2))],[self.x,WINDOW_HEIGHT-(self.y+(self.length/2))],self.thickness)
+        self.y = WINDOW_HEIGHT-mousey
+        pygame.draw.line(screen, colors["paddle"],[self.x,self.y-(self.length/2)],[self.x,self.y+(self.length/2)],self.thickness)
 
-Ball = Ball(random.randrange(10,WINDOW_WIDTH-10),random.randrange(10,WINDOW_HEIGHT-10),10,5) #게임공
+Ball = Ball(random.randrange(10,WINDOW_WIDTH-10),random.randrange(10,WINDOW_HEIGHT-10),10,4) #게임공
 PaddleTOP = Paddle(random.randrange(0,WINDOW_WIDTH),30,100,14) #위쪽 패들
 PaddleBOTTOM = Paddle(random.randrange(0,WINDOW_WIDTH),WINDOW_HEIGHT-30,100,14) #아랫쪽 패들
 PaddleLEFT = Paddle(30,random.randrange(0,WINDOW_HEIGHT),100,14) #왼쪽 패들
